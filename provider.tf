377a91b285ff8041c34659a53f48d7206cfcfc2c
mac <mac@Sammys-MacBook-Pro.local>
2026-04-21 05:25:19 +0100

    Add S3 backend for shared Terraform state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "samuel-terraform-state-2094"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
